479caf608e8933a3dbc682ebb87812ef5a659fee
purcellc693 <purcellc693@cis8.strose.edu>
2016-09-26 20:57:37 -0400

    comment is to show corey chanegd the file

--- hello.hs
+++ hello.hs
@@ -1,10 +1,10 @@
 // this comment was made from inside the cloned repository
-//corey purcell made this change 8:43pm
+
 module Main where
 
 main = putStrLn "Hello Cody Husek!"
-//corey purcell made this change 8:43pm-----
-//why is this not working
+//corey purcell made this change 8:56pm-----
+
 
 
 
